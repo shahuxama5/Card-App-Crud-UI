36252363da501d3d2a1fe2b33b67d1db7c7057d5
Usama Mehmood <usamamehmood62000@gmail.com>
2022-04-18 06:12:48 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { CardService } from './service/card.service';
+
+import { Component, OnInit } from '@angular/core';
+import { Card } from './models/Card.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Cards';
+  cards: Card[] = [];
+  card: Card = {
+    id: '',
+    cardHolderName: '',
+    cardNumber: '',
+    cvc: '',
+    expiryMonth: '',
+    expiryYear: ''
+  }
+
+  constructor(private cardService: CardService) {
+
+  }
+  ngOnInit(): void {
+    this.getAllCards();
+  }
+
+  getAllCards() {
+    this.cardService.getAllCards().subscribe( res => {
+      this.cards = res;
+    });
+  }
+
+  onSubmit() {
+    if (this.card.id === '') {
+      this.cardService.addCard(this.card).subscribe( res => {
+        this.getAllCards();
+        this.card = {
+          id: '',
+          cardHolderName: '',
+          cardNumber: '',
+          cvc: '',
+          expiryMonth: '',
+          expiryYear: ''
+        }
+      });
+    }
+    else {
+      this.updateCard(this.card);
+    }
+  }
+
+  deleteCard(id: string) {
+    this.cardService.deleteCard(id).subscribe( res => {
+      this.getAllCards();
+    });
+  }
+
+  populateForm(card: Card) {
+    this.card = card;
+  }
+
+  updateCard(card: Card) {
+    this.cardService.updateCard(card).subscribe( res => {
+      this.getAllCards();
+    });
+  }
 }
+
